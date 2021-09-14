753d5798f974eefa6325e58ce26aac092d942579
Berkay <34893128+raufsamestone@users.noreply.github.com>
2021-09-14 12:58:46 +0300

    Update _document.js

--- _document.js
+++ _document.js
@@ -10,18 +10,39 @@ export default class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
+
+           <script
+              async
+            src='https://www.googletagmanager.com/gtag/js?id=UA-202369355-1'
+            />
+          
           <script
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', 'UA-202369355-1' {
               page_path: window.location.pathname,
             });
           `,
             }}
           />
+          
+     
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config','UA-202369355-1' {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+           />
+       
         </Head>
         <body>
           <Main />
