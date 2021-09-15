2255f1841c0b7deb4fb472fe533597c79368ab91
Berkay <34893128+raufsamestone@users.noreply.github.com>
2021-09-15 09:06:23 +0300

    Update ProductView.tsx

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -65,18 +65,24 @@ const ProductView: FC<Props> = ({ product }) => {
             async
             dangerouslySetInnerHTML={{
               __html: `
+              
+// Measure a view of product details. This example assumes the detail view occurs on pageload,
+// and also tracks a standard pageview of the details page.
 dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
 dataLayer.push({
-  'event': 'removeFromCart',
   'ecommerce': {
-    'remove': {                               // 'remove' actionFieldObject measures.
-      'products': [{                          //  removing a product to a shopping cart.
-          'name': product.name,
-          'id': product.id,
-          'price': product.price,
-      }]
-    }
-  }
+    'detail': {
+      'actionField': {'list': 'Apparel Gallery'},    // 'detail' actions have an optional list property.
+      'products': [{
+        'name': 'Triblend Android T-Shirt',         // Name or ID is required.
+        'id': '12345',
+        'price': '15.25',
+        'brand': 'Google',
+        'category': 'Apparel',
+        'variant': 'Gray'
+       }]
+     }
+   }
 });
 
 `
