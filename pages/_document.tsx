89d2d9b2685498d6144b1cfbdc2cca109d0a2d94
Berkay <34893128+raufsamestone@users.noreply.github.com>
2021-10-10 13:28:05 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,38 +4,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-<!--          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-        
-           <script
-            dangerouslySetInnerHTML={{
-              __html: `  
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-202369355-1"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-202369355-1');
-          </script>`
-             }}
-          />
- -->
-        
-<!-- Segment Snippet -->
         
          <script
             dangerouslySetInnerHTML={{
