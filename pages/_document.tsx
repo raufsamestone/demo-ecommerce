188f93c70d385d5521f65849f2237b04c2a1208b
Berkay <34893128+raufsamestone@users.noreply.github.com>
2021-10-10 13:23:13 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-         <script
+<!--          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
@@ -33,8 +33,11 @@ class MyDocument extends Document {
           </script>`
              }}
           />
-
-      <script
+ -->
+        
+<!-- Segment Snippet -->
+        
+         <script
             dangerouslySetInnerHTML={{
               __html: `  
           !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="Axypk01si052s7DAx9jDQB5VLeo2NDMn";;analytics.SNIPPET_VERSION="4.15.3";
@@ -45,6 +48,7 @@ class MyDocument extends Document {
           `
              }}
           />
+        
         <Head />
         <body className="loading">
           <Main />
