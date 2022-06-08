7394fbb301d9b838e337c0bfeefdd108900dc95b
Berkay <34893128+raufsamestone@users.noreply.github.com>
2022-06-08 12:55:43 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,9 +32,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
              }}
           />
           
-       
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMHXJ2D"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
           <Main />
           <NextScript />
