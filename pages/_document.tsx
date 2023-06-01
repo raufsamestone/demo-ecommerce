a3d3da548d3355b369fbc903e68897bd3c3bdda4
Berkay <34893128+raufsamestone@users.noreply.github.com>
2023-06-01 14:08:11 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NMHXJ2D');
+})(window,document,'script','dataLayer','GTM-N954D8W');
 <!-- End Google Tag Manager -->
        </script>
           `
@@ -26,7 +26,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
          <noscript
             dangerouslySetInnerHTML={{
               __html: `  
-<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMHXJ2D"
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N954D8W"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>
           `
             }}
